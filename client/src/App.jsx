71658fe8f7a81e8fea3ba92b6e9528e1100451f7
abuzar <abuzardawar444@gmail.com>
2024-12-09 20:35:28 +0500

    `Remove unused import and dev tools toggle functionality`

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
@@ -114,22 +114,11 @@ const router = createBrowserRouter([
   }
 ])
 
-const ReactQueryDevToolsProduction = React.lazy(() => import('@tanstack/react-query-devtools/build/lib/index.prod.js').then((d) => ({ default: d.ReactQueryDevtools })))
-
 const App = () => {
-  const [showDevTools, setShowDevTools] = useState(false)
-  React.useEffect(() => {
-    window.toggleDevtools = () => setShowDevTools((old) => !old)
-  }, [])
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false}/>
-      {showDevTools && (
-        <React.Suspense fallback={null}>
-          <ReactQueryDevToolsProduction />
-        </React.Suspense>
-      )}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
