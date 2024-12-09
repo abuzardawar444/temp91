0a02a94c7b2638e1e9bf29fe95fff7707c61e26d
abuzar <abuzardawar444@gmail.com>
2024-12-09 20:36:30 +0500

    `Update import statements and remove unused code`

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,118 +1,115 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import {
-  HomeLayout, 
-  Login,
+  HomeLayout,
+  Landing,
   Register,
+  Login,
   DashboardLayout,
   Error,
-  Landing,
   AddJob,
   Stats,
   AllJobs,
   Profile,
+  Admin,
   EditJob,
-  Admin
-} from './pages'
+} from './pages';
 
-import {action as registerAction} from './pages/Register.jsx';
-import {action as loginAction} from './pages/Login.jsx';
-import {loader as dashboardLoader} from './pages/DashboardLayout.jsx'
-import {action as addJobAction} from './pages/AddJob.jsx'
-import {loader as allJobsLoader} from './pages/AllJobs.jsx'
-import {action as editJobAction} from './pages/EditJob.jsx'
-import {loader as editJobLoader} from './pages/EditJob.jsx'
-import {action as deleteJobAction} from './pages/DeleteJob.jsx'
-import {loader as adminLoader} from './pages/Admin.jsx'
-import {action as profileAction} from './pages/Profile.jsx'
-import {loader as statsLoader} from './pages/Stats.jsx'
-import ErrorElement from './components/ErrorElement.jsx'
+import { action as registerAction } from './pages/Register';
+import { action as loginAction } from './pages/Login';
+import { loader as dashboardLoader } from './pages/DashboardLayout';
+import { action as addJobAction } from './pages/AddJob';
+import { loader as allJobsLoader } from './pages/AllJobs';
+import { loader as editJobLoader } from './pages/EditJob';
+import { action as editJobAction } from './pages/EditJob';
+import { action as deleteJobAction } from './pages/DeleteJob';
+import { loader as adminLoader } from './pages/Admin';
+import { action as profileAction } from './pages/Profile';
+import { loader as statsLoader } from './pages/Stats';
+import ErrorElement from './components/ErrorElement';
 
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
   document.body.classList.toggle('dark-theme', isDarkTheme);
   return isDarkTheme;
-}
+};
+
 checkDefaultTheme();
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime:1000 * 60 * 5,
-    }
-  }
-})
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
+
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomeLayout/>,
-    errorElement: <Error/>,
+    element: <HomeLayout />,
+    errorElement: <Error />,
     children: [
-      // To show other pages on paths relative to home layout.
       {
-        // To show landing page on home layout
         index: true,
-        element: <Landing/>
+        element: <Landing />,
       },
       {
         path: 'register',
-        element: <Register/>,
+        element: <Register />,
         action: registerAction,
       },
       {
         path: 'login',
-        element: <Login/>,
+        element: <Login />,
         action: loginAction(queryClient),
       },
       {
         path: 'dashboard',
-        element: <DashboardLayout queryClient={queryClient}/>,
+        element: <DashboardLayout queryClient={queryClient} />,
         loader: dashboardLoader(queryClient),
         children: [
           {
             index: true,
-            element: <AddJob/>,
+            element: <AddJob />,
             action: addJobAction(queryClient),
           },
           {
             path: 'stats',
-            element: <Stats/> ,
+            element: <Stats />,
             loader: statsLoader(queryClient),
-            errorElement: <ErrorElement/>
+            errorElement: <ErrorElement />,
           },
           {
             path: 'all-jobs',
-            element: <AllJobs/>,
+            element: <AllJobs />,
             loader: allJobsLoader(queryClient),
-            errorElement: <ErrorElement/>
+            errorElement: <ErrorElement />,
           },
           {
             path: 'profile',
-            element: <Profile/>,
-            action: profileAction(queryClient)
+            element: <Profile />,
+            action: profileAction(queryClient),
           },
           {
             path: 'admin',
-            element: <Admin/> ,
+            element: <Admin />,
             loader: adminLoader,
           },
           {
             path: 'edit-job/:id',
-            element: <EditJob/>,
+            element: <EditJob />,
             loader: editJobLoader(queryClient),
             action: editJobAction(queryClient),
           },
-          {
-            path: 'delete-job/:id',
-            action: deleteJobAction(queryClient)
-          }
-        ]
+          { path: 'delete-job/:id', action: deleteJobAction(queryClient) },
+        ],
       },
-    ]
-  }
-])
+    ],
+  },
+]);
 
 const App = () => {
   return (
@@ -121,6 +118,5 @@ const App = () => {
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
-}
-
-export default App
+};
+export default App;
